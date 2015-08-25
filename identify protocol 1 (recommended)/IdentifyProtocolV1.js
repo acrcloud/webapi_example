2ab19472e3f81100bbc2a046a875af9658eac96b
olym <olym@acrcloud.com>
2015-08-25 20:39:03 +0800

    add nodejs identify example

---
+++ identify protocol 1 (recommended)/IdentifyProtocolV1.js
@@ -0,0 +1,65 @@
+var url = require('url');
+var fs = require('fs');
+var crypto = require('crypto');
+//npm install request
+var request = require('request');
+
+var defaultOptions = {
+  host: 'ap-southeast-1.api.acrcloud.com',
+  endpoint: '/v1/identify',
+  signature_version: '1',
+  data_type:'audio',
+  secure: true,
+  access_key: 'your_access_key',
+  access_secret: 'your_access_key'
+};
+
+function buildStringToSign(method, uri, accessKey, dataType, signatureVersion, timestamp) {
+  return [method, uri, accessKey, dataType, signatureVersion, timestamp].join('\n');
+}
+
+function sign(signString, accessSecret) {
+  return crypto.createHmac('sha1', accessSecret)
+    .update(new Buffer(signString, 'utf-8'))
+    .digest().toString('base64');
+}
+
+/**
+ * Identifies a sample of bytes
+ */
+function identify(data, options, cb) {
+
+  var current_data = new Date();
+  var timestamp = current_data.getTime()/1000;
+
+  var stringToSign = buildStringToSign('POST',
+    options.endpoint,
+    options.access_key,
+    options.data_type,
+    options.signature_version,
+    timestamp);
+
+  var signature = sign(stringToSign, options.access_secret);
+
+  var formData = {
+    sample: data,
+    access_key:options.access_key,
+    data_type:options.data_type,
+    signature_version:options.signature_version,
+    signature:signature,
+    sample_bytes:data.length,
+    timestamp:timestamp,
+  }
+  request.post({
+    url: "http://"+options.host + options.endpoint,
+    method: 'POST',
+    formData: formData
+  }, cb);
+}
+
+var bitmap = fs.readFileSync('sample.wav');
+
+identify(new Buffer(bitmap), defaultOptions, function (err, httpResponse, body) {
+  if (err) console.log(err);
+  console.log(body);
+});
---
+++ identify protocol 2/IdentifyProtocolV2.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var crypto = require('crypto');
+var fs = require("fs");
+var qs = require('querystring');  
+
+function create_sign(data, secret_key) {
+    return crypto.createHmac('sha1', secret_key).update(data).digest().toString('base64');
+}
+
+function recogize(host, access_key, secret_key, query_data, query_type) {
+    var http_method = "POST"
+    var http_uri = "/v1/identify"
+    var data_type = query_type
+    var signature_version = "1" 
+    var current_data = new Date();
+    var minutes = current_data.getTimezoneOffset();
+    var timestamp = parseInt(current_data.getTime()/1000) + minutes*60 + '';
+    var sample_bytes = query_data.length + '';
+
+    var options = {
+        hostname: host,
+        port: 80,
+        path: http_uri,
+        method: http_method,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    };
+
+    var string_to_sign = http_method+"\n"+http_uri+"\n"+access_key+"\n"+data_type+"\n"+signature_version+"\n"+timestamp;
+    var sign = create_sign(string_to_sign, secret_key);
+    var post_data = {
+        'access_key':access_key, 
+        'sample_bytes':sample_bytes, 
+        'sample':query_data.toString('base64'),
+        'timestamp':timestamp, 
+        'signature':sign, 
+        'data_type':data_type, 
+        'signature_version':signature_version
+    };
+
+    var content = qs.stringify(post_data); 
+
+    var req = http.request(options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    });
+
+    req.on('error', function (e) {
+        console.log('problem with request: ' + e.message);
+    });
+
+    req.write(content);
+    req.end();
+}
+
+var your_access_key = "your_access_key";
+var your_access_secret = "your_access_secret";
+var host = "ap-southeast-1.api.acrcloud.com";
+var data_type = 'audio';
+var bitmap = fs.readFileSync('sample.wav');
+recogize(host, your_access_key, your_access_secret, new Buffer(bitmap), data_type);
