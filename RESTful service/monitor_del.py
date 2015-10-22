93502a3ca3dce29727a042da62cec02f83ed610c
olym <olym@acrcloud.com>
2015-10-22 11:12:30 +0800

    add monitor add and delete

---
+++ RESTful service/monitor_del.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import base64
+import hmac
+import hashlib
+import urllib
+import time
+import requests
+
+'''
+This demo shows how to use the RESTful API to upload an audio file ( "data_type":"audio" ) into your bucket.
+You can find account_access_key and account_access_secret in your account page.
+Log into http://console.acrcloud.com -> "Account" (top right corner) -> "RESTful API Keys" -> "Create Key Pair". 
+Be Careful, they are different with access_key and access_secret of your project.
+'''
+account_access_key = "3ab8187eea92c842"
+account_access_secret = "475723efd40bb3160b3163971343a7ef"
+
+requrl = "https://api.acrcloud.com/v1/monitors/1447"
+http_method = "DELETE"
+http_uri = "/v1/monitors/1447"
+signature_version = "1"
+timestamp = time.time()
+
+string_to_sign = http_method+"\n"+http_uri+"\n"+account_access_key+"\n"+signature_version+"\n"+str(timestamp)
+
+sign = base64.b64encode(
+        hmac.new(account_access_secret, string_to_sign, digestmod=hashlib.sha1)
+        .digest())
+
+headers = {'access-key': account_access_key, 'signature-version': signature_version, 'signature': sign, 'timestamp':timestamp}
+
+r = requests.delete(requrl, headers=headers, verify=True)
+r.encoding = "utf-8"
+print r.text
