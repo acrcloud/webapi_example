f156156c6d26abed88254ac21974978078a6bfed
olym <olym@acrcloud.com>
2016-01-05 16:02:55 +0800

    add project

--- RESTful service/project.py
+++ RESTful service/project.py
@@ -10,8 +10,8 @@ import requests
 option = {
   'host': 'api.acrcloud.com',
   'signature_version': '1',
-  'access_key': '<your account access key>', 
-  'access_secret': '<your account access secret>'
+  'access_key': '<your account access key>',
+  'access_secret': '<your account access secret>',
 };
 
 
@@ -20,10 +20,10 @@ def sign(string_to_sign, access_secret):
 		    hmac.new(access_secret, string_to_sign, digestmod=hashlib.sha1)
 		    .digest())
 	
-def create_bucket(name, type, scale, content_type):
+def create_project(name, type, buckets, audio_type, external_id):
     http_method = "POST"
     timestamp = time.time()
-    uri = '/v1/buckets'
+    uri = '/v1/projects'
     
     string_to_sign = '\n'.join((http_method, uri, option['access_key'], option['signature_version'], str(timestamp)))
 
@@ -31,17 +31,17 @@ def create_bucket(name, type, scale, content_type):
  
     headers = {'access-key': option['access_key'], 'signature-version': option['signature_version'], 'signature': signature, 'timestamp':timestamp}
 
-    data = {'name':name, 'type':type, 'scale':scale,'content_type':content_type}
+    data = {'name':name, 'type':type, 'buckets':buckets, 'audio_type':audio_type, 'external_id':external_id}
     
     requrl = "https://"+option['host'] + uri
     r = requests.post(requrl, data=data, headers=headers, verify=True)
     r.encoding = "utf-8"
     print r.text
 
-def delete_bucket(name):
+def delete_project(name):
     http_method = "DELETE"
     timestamp = time.time()
-    uri = '/v1/buckets'+"/"+name
+    uri = '/v1/projects'+"/"+name
     
     string_to_sign = '\n'.join((http_method, uri, option['access_key'], option['signature_version'], str(timestamp)))
 
@@ -57,5 +57,5 @@ def delete_bucket(name):
     print r.text
 
 if __name__ == "__main__":
-    create_bucket('test_api_bucket', 'File', 100, "Music")
-    #delete_bucket('test_api_bucket')
+    create_project('test_api_project', 'AVR', "asdf", 1, "deezer,itunes,spotify")
+    delete_project('test_api_project')
