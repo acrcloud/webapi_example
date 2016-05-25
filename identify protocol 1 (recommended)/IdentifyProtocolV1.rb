c2e189f2f2b0e5376b51b70f5d17907fddea95ab
acrcloudsteve <steve@acrcloud.com>
2016-05-25 11:33:16 +0800

    Update IdentifyProtocolV1.rb

--- identify protocol 1 (recommended)/IdentifyProtocolV1.rb
+++ identify protocol 1 (recommended)/IdentifyProtocolV1.rb
@@ -2,10 +2,11 @@ require 'openssl'
 require 'base64'
 require 'net/http/post/multipart'
 
-access_key = "xxxxxxxx"
-access_secret = "xxxxxxxx"
 
-requrl = "http://ap-southeast-1.api.acrcloud.com/v1/identify"
+access_key = "###YOUR_ACCESS_KEY###"
+access_secret = "###YOUR_ACCESS_SECRET###"
+requrl = "http://###YOUR_HOST###/v1/identify"
+
 http_method = "POST"
 http_uri = "/v1/identify"
 data_type = "audio"
