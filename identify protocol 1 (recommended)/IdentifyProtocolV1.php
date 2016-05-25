ea0048345ca1c9df72979e894cc31f97bbb04905
acrcloudsteve <steve@acrcloud.com>
2016-05-25 11:30:53 +0800

    Update IdentifyProtocolV1.php

--- identify protocol 1 (recommended)/IdentifyProtocolV1.php
+++ identify protocol 1 (recommended)/IdentifyProtocolV1.php
@@ -1,5 +1,5 @@
 <?php
-$requrl = "http://ap-southeast-1.api.acrcloud.com/v1/identify";
+
 $http_method = "POST";
 $http_uri = "/v1/identify";
 $data_type = "audio";
@@ -7,9 +7,10 @@ $signature_version = "1" ;
 $timestamp = time() ;
 
 
-// Replace "xxxxxxxx" below with your project's access_key and access_secret.
-$access_key =  'xxxxxxxx';
-$access_secret =  'xxxxxxxx';
+// Replace "###...###" below with your project's host, access_key and access_secret.
+$requrl = "###YOUR_HOST###/v1/identify";
+$access_key =  '###YOUR_ACCESS_KEY###';
+$access_secret =  '###YOUR_ACCESS_SECRET###';
 
 $string_to_sign = $http_method . "\n" . 
                   $http_uri ."\n" . 
