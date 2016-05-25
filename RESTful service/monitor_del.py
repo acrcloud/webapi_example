01a436ce18828ef6e53ffffe2cd4198e0e2c4cec
acrcloudsteve <steve@acrcloud.com>
2016-05-25 11:51:53 +0800

    Update monitor_del.py

--- RESTful service/monitor_del.py
+++ RESTful service/monitor_del.py
@@ -13,12 +13,12 @@ You can find account_access_key and account_access_secret in your account page.
 Log into http://console.acrcloud.com -> "Account" (top right corner) -> "RESTful API Keys" -> "Create Key Pair". 
 Be Careful, they are different with access_key and access_secret of your project.
 '''
-account_access_key = "3ab8187eea92c842"
-account_access_secret = "475723efd40bb3160b3163971343a7ef"
+account_access_key = "###YOUR_ACCESS_KEY###"
+account_access_secret = "###YOUR_ACCESS_SECRET###"
 
-requrl = "https://api.acrcloud.com/v1/monitor-streams/1447"
+requrl = "https://api.acrcloud.com/v1/monitor-streams/###STREAM_ID###"
 http_method = "DELETE"
-http_uri = "/v1/monitor-streams/1447"
+http_uri = "/v1/monitor-streams/###STREAM_ID###"
 signature_version = "1"
 timestamp = time.time()
 
