54fca36a2474d3c4a059053d25883dbfac22e18f
olym <olym@acrcloud.com>
2017-03-15 19:16:29 +0800

    Update channel.py

--- RESTful service/channel.py
+++ RESTful service/channel.py
@@ -93,9 +93,35 @@ def delete_channel(acr_id):
     r.encoding = "utf-8"
     if r.status_code == 204:
         print "deleted"
+	
+def update_channel(acr_id, channel_url, title, channel_id, custom_fields=None):
+    http_method = "PUT"
+    timestamp = time.time()
+    http_uri = "/v1/channels/"+acr_id
+
+    string_to_sign = '\n'.join((http_method, http_uri, option['access_key'], option['signature_version'], str(timestamp)))
+
+    signature = sign(string_to_sign, option['access_secret'])
+
+    headers = {'access-key': option['access_key'], 'signature-version': option['signature_version'], 'signature': signature, 'timestamp':timestamp}
+    data = {'url':channel_url,'title':title, "channel_id":channel_id}
+    if custom_fields:
+        keys = []
+        values = []
+        for k in custom_fields:
+            keys.append(k)
+            values.append(custom_fields[k])
+        data['custom_key[]'] = keys
+        data['custom_value[]'] = values
+
+    requrl = "https://"+option['host'] + http_uri
+    r = requests.put(requrl, data=data, headers=headers, verify=True)
+    r.encoding = "utf-8"
+    print r.text
 
 if __name__ == "__main__":
     #create_channel('eu_test', 'http://127.0.0.1', 'channel 1', '12345')
+    update_channel("722ad0163be810eb972c1f619aa1f1b3", 'http://127.0.0.1', 'channel 1', '12345', {"aaa":"223", "bbb":"222"})
     delete_channel("722ad0163be810eb972c1f619aa1f1b3")
     #get_all_channels('eu_test', 1)
     #get_channel('b6c9985babb2e9ecb1642d04a7b149d8')
