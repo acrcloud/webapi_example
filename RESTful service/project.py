72af8afb2723455e7c2b3e174b9f03652eaf5186
olym <olym@acrcloud.com>
2017-03-22 00:26:30 +0800

    Update project.py

--- RESTful service/project.py
+++ RESTful service/project.py
@@ -6,6 +6,7 @@ import hashlib
 import urllib
 import time
 import requests
+import json
 
 option = {
   'host': 'api.acrcloud.com',
@@ -31,7 +32,7 @@ def create_project(name, region, type, buckets, audio_type, external_id):
  
     headers = {'access-key': option['access_key'], 'signature-version': option['signature_version'], 'signature': signature, 'timestamp':timestamp}
 
-    data = {'name':name, 'region':region, 'type':type, 'buckets':buckets, 'audio_type':audio_type, 'external_id':external_id}
+    data = {'name':name, 'region':region, 'type':type, 'buckets':json.dumps(buckets), 'audio_type':audio_type, 'external_id':external_id}
     
     requrl = "https://"+option['host'] + uri
     r = requests.post(requrl, data=data, headers=headers, verify=True)
@@ -43,16 +44,16 @@ def update_project(name, buckets):
     http_method = "PUT"
     timestamp = time.time()
     uri = '/v1/projects/'+name
-
+    
     string_to_sign = '\n'.join((http_method, uri, option['access_key'], option['signature_version'], str(timestamp)))
 
     signature = sign(string_to_sign, option['access_secret'])
-
+ 
     headers = {'access-key': option['access_key'], 'signature-version': option['signature_version'], 'signature': signature, 'timestamp':timestamp}
 
-    data = {'buckets':buckets}
-
-    requrl = "https://"+option['host'] + uri
+    data = {'buckets':json.dumps(buckets)}
+    
+    requrl = "https://"+option['host'] + uri 
     r = requests.put(requrl, data=data, headers=headers, verify=True)
     r.encoding = "utf-8"
     print r.text
@@ -105,8 +106,8 @@ def list_projects():
 
 
 if __name__ == "__main__":
-    create_project('test_api_project', 'us-west-2', 'AVR', "test_api_bucket", 1, "")
-    get_project('test_api_project')
-    update_project('test_api_project', "ACRCloud Music")
-    delete_project('test_api_project')
-    list_projects()
+    create_project('test_api_project', 'us-west-2', 'AVR', [{"name":"ACRCloud Music"}, {"name":"usbucket"}], 1, "")
+    #get_project('test_api_project')
+    #update_project('test_api_project5', [{"name":"ACRCloud Music"}, {"name":"us_bucket", "id":1159}])
+    #delete_project('test_api_project')
+    #list_projects()
