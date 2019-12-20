0d80c8f52396154e119a99fef49687bf4cde0a19
ICYMYM <qiang@acrcloud.com>
2019-12-20 20:10:54 +0800

    update broadcast_database

--- RESTful service/monitor_api_for_broadcast_database.py
+++ RESTful service/monitor_api_for_broadcast_database.py
@@ -40,6 +40,51 @@ class Acrcloud_Monitor_API:
         }
         return headers
 
+    def get_projects(self):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/projects"
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "GET"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        r = requests.get(requrl, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
+    def set_result_callback(self, project_name, result_callback_url, send_noresult=False, post_data_type="json", result_type="realtime"):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/projects/result_callback"
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "POST"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        data = {
+            "project_name":project_name,
+            "url":result_callback_url,
+            "send_noresult":send_noresult,
+            "post_data_type":post_data_type,
+            "result_type":result_type,
+        }
+        r = requests.post(requrl, data=data, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
+    def set_state_callback(self, project_name, state_callback_url, post_data_type="json"):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/projects/state_callback"
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "POST"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        data = {
+            "project_name":project_name,
+            "url":state_callback_url,
+            "post_data_type":post_data_type,
+        }
+        r = requests.post(requrl, data=data, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
     def get_project_channels(self, project_name, page_num=1):
         requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams"
         http_uri = requrl[requrl.find("/v1/"):]
@@ -75,12 +120,100 @@ class Acrcloud_Monitor_API:
         r.encoding = "utf-8"
         return r.text
 
+    def get_channel_urls(self, channel_id):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/{0}/urls".format(channel_id)
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "GET"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        r = requests.get(requrl, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
+    def del_channel_urls(self, channel_id, del_url_list):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/{0}/urls".format(channel_id)
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "DELETE"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        data = {"del_urls":json.dumps(del_url_list)}
+        r = requests.delete(requrl, data=data, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
+    def add_channel_urls(self, channel_id, add_url_list):
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/{0}/urls".format(channel_id)
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "POST"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        params = {"add_urls":json.dumps(add_url_list)}
+        r = requests.post(requrl, data=data, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        return r.text
+
+    def get_recording(self, access_key, channel_id, record_timestamp, played_duration):
+        "GET,HEAD <module>/acrcloud-monitor-streams/recording/"
+        requrl = "https://api.acrcloud.com/v1/acrcloud-monitor-streams/recording/{0}/{1}".format(access_key, channel_id)
+        http_uri = requrl[requrl.find("/v1/"):]
+        http_method = "GET"
+        signature_version = "1"
+
+        headers = self.create_headers(http_uri, http_method, signature_version)
+        params = {"record_timestamp":record_timestamp, "played_duration":played_duration}
+        r = requests.get(requrl, params=params, headers=headers, verify=True)
+        try:
+            d = r.headers['content-disposition']
+            fname = d[ (d.find('filename="') + len('filename="')) : d.find('";') ]
+            fname = fname.replace(":", "_")
+        except Exception as e:
+            #print ("Error@get_recording: {0}".format(str(e)))
+            fname = "acrcloud_{0}_{1}_{2}.failed".format(channel_id, record_timestamp, played_duration)
+        return fname, r.content
+
+
 
 class Acrcloud_Monitor_Demo:
 
     def __init__(self, config):
         self.config = config
         self.api = Acrcloud_Monitor_API(self.config["account_access_key"], self.config["account_access_secret"])
+
+    def projects(self):
+        try:
+            info = self.api.get_projects()
+            jinfo = json.loads(info)
+            return jinfo["data"]
+        except Exception as e:
+            traceback.print_exc()
+        return []
+
+    def set_state_callback(self, project_name, state_callback_url, post_data_type="json"):
+        """
+        post_data_type: "json" or "form"
+        """
+        try:
+            if state_callback_url:
+                return self.api.set_state_callback(project_name, state_callback_url, post_data_type)
+        except Exception as e:
+            traceback.print_exc()
+        return None
+
+    def set_result_callback(self, project_name, result_callback_url, send_noresult=False, post_data_type="json", result_type="realtime"):
+        """
+        send_noresult: True or False
+        post_data_type: "json" or "form"
+        result_type: "realtime" or "delay"
+        """
+        try:
+            if result_callback_url:
+                return self.api.set_result_callback(project_name, result_callback_url, send_noresult, post_data_type, result_type)
+        except Exception as e:
+            traceback.print_exc()
+        return None
 
     def all_project_channels(self, project_name):
         try:
@@ -108,23 +241,66 @@ class Acrcloud_Monitor_Demo:
         jresults = json.loads(results)
         return jresults
 
+    def get_channel_urls(self, channel_id):
+        urls = self.api.get_channel_urls(channel_id)
+        jurls = json.loads(urls)
+        return jurls
+
+    def add_channel_urls(self, channel_id, add_urls):
+        ret = self.api.add_channel_urls(channel_id, add_urls)
+        return ret
+
+    def del_channel_urls(self, channel_id, del_urls):
+        ret = self.api.del_channel_urls(channel_id, del_urls)
+        return ret
+
+    def get_recording(self, access_key, channel_id, record_timestamp, played_duration):
+        fname, content = self.api.get_recording(access_key, channel_id, record_timestamp, played_duration)
+        return fname, content
+
 if __name__ == "__main__":
     config = {
         "account_access_key" : "XXXX",
-        "account_access_secret" : "XXXXXX",
+        "account_access_secret" : "XXXXXXXXXX",
     }
 
     ams = Acrcloud_Monitor_Demo(config)
 
+    #Get all the projects
+    project_list = ams.projects()
+
+    #Set State Callback_URL
+    #post_data_type: "json" or "form"
+    ams.set_state_callback("<project_name>", "<state_callback_url>", "json")
+
+    #Set Result Callback_URL
+    #send_noresult: True or False
+    #post_data_type: "json" or "form"
+    #result_type: "realtime" or "delay"
+    ams.print ams.set_result_callback("<project_name>", "<result_callback_url>", False, "form", "realtime")
+
+    """
     project_name = "<your project name>"
     print ams.all_project_channels(project_name)
 
     channel_id = "<acrcloud db channel id>"
     print ams.channel_info(channel_id)
 
-    project_name = "<your project name>"
-    channel_id = "<acrcloud db channel id>"
-    date = "20190228"
-    rlist = ams.channel_results(project_name, channel_id, date)
-    for item in rlist:
-        print item
+    channel_id = "XXXXX"
+    print ams.get_channel_urls(channel_id)
+    add_url_list = ["url1"]
+    print ams.add_channel_urls(channel_id, add_url_list)
+    print ams.get_channel_urls(channel_id)
+    del_url_list = ["url1"]
+    print ams.del_channel_urls(channel_id, del_url_list)
+
+    access_key = "<your project access_key>"
+    channel_id = "channel_id"
+    record_timestamp = "20191203131312"
+    played_duration = 100
+    fname, fcontent = ams.get_recording(access_key, channel_id, record_timestamp, played_duration)
+    if not fname.endswith("failed"):
+        with open(fname, "wb") as wfile:
+            wfile.write(fcontent)
+
+    """
